0d45ef306a9ee325ba20bcf1040b5be9db743c57
Salmix1326 <shapasasha@gmail.com>
2025-10-06 21:06:36 +0200

    Tasks for Practice42

--- client.py
+++ client.py
@@ -10,22 +10,83 @@
 #     print(response.text)
 
 
+# import requests
+#
+#
+# response = requests.get("http://localhost:8000/greeting")
+#
+# if response.ok:
+#     data = response.json()
+#     print(data)
+# else:
+#     print(response.text)
+#
+# response = requests.get("http://localhost:8001/greeting")
+#
+# if response.ok:
+#     data = response.json()
+#     print(data)
+# else:
+#     print(response.text)
+
+# import requests
+#
+#
+# response = requests.post("http://localhost:8000/hello/Mary")
+#
+# if response.ok:
+#     data = response.json()
+#     print(data)
+# else:
+#     print(response.text)
+#
+# name_data = {
+#     "name": "John"
+# }
+#
+# response = requests.post("http://localhost:8000/hello_json", json=name_data)
+#
+# if response.ok:
+#     data = response.json()
+#     print(data)
+# else:
+#     print(response.text)
+
 import requests
 
 
-response = requests.get("http://localhost:8000/greeting")
+while True:
+    user_choice = input('Choose option\nTo get all books press (1)\nTo add new book press (2)').strip()
 
-if response.ok:
-    data = response.json()
-    print(data)
-else:
-    print(response.text)
+    if user_choice == '1':
+        response = requests.get('http://localhost:8000/books')
 
-response = requests.get("http://localhost:8001/greeting")
+        if response.ok:
+            data = response.json()
 
-if response.ok:
-    data = response.json()
-    print(data)
-else:
-    print(response.text)
+            print(data)
+        else:
+            print(response.text)
+
+    elif user_choice == '2':
+        book_id = int(input('Please enter book\'s id: ').strip())
+        book_title = input('Please enter book\'s title: ').strip()
+        book_author = input('Please enter book\'s author: ').strip()
+        book_year = int(input('Please enter book\'s year: ').strip())
+        book_pages = int(input('Please enter book\'s pages quantity: ').strip())
+
+        new_book = {
+            "id": book_id,
+            "title": book_title,
+            "author": book_author,
+            "year": book_year,
+            "pages": book_pages
+        }
+
+        response = requests.post('http://localhost:8000/books', json=new_book)
+
+        if response.ok:
+            print('Book is added')
+        else:
+            print(response.text)
 
--- main.py
+++ main.py
@@ -35,39 +35,18 @@
 # ● порт – 8001
 # Запустіть обида сервери на localhost
 # Напишіть клієнта який робить запита на обидва сервери
-from fastapi import FastAPI
-
-
-app = FastAPI()
-
-@app.get("/greeting")
-def hi():
-    return {"respond": "Привіт з сервера1"}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# from fastapi import FastAPI
+#
+#
+# app = FastAPI()
+#
+# @app.get("/greeting")
+# def hi():
+#     return {"respond": "Привіт з сервера1"}
 
 
 # Завдання 3
+#===========================================================================
 # Напишіть сервер з такими функціями
 # ● hello
 # ○ шлях – /hello/{name}
@@ -77,10 +56,28 @@ def hi():
 # ○ шлях – /hello_json
 # ○ метод – POST
 # ○ повертає {"message": "Привіт, {ім'я}!"}
-# Для hello_json напишіть модель за допомогою
-# pydantic
+# Для hello_json напишіть модель за допомогою pydantic
 # Запустіть сервер
 # Напишіть клієнта який робить запити на сервер
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+#
+#
+# app = FastAPI()
+#
+# class NameModel(BaseModel):
+#     name: str
+#
+# @app.post("/hello/{name}")
+# def hello(name: str):
+#     return {"message": f"Привіт, {name}!"}
+#
+#
+# @app.post("/hello_json")
+# def hello_json(data: NameModel):
+#     return {"message": f"Привіт, {data.name}!"}
+
+
 # Завдання 4
 # Напишіть сервер для симуляції роботи бібліотеки.
 # Дані про книги знаходяться у файлі books.json
@@ -104,3 +101,50 @@ def hi():
 # 4. Видалення книги за ID
 # ○ шлях – books/{book_id}
 # ○ метод – DELETE
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+import os
+
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    year: int
+    pages: int
+
+app = FastAPI()
+
+
+@app.get("/books")
+def get_all_books(filename: str = 'books.json'):
+    if not os.path.exists(filename):
+        return {}
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            data = {}
+
+    return data
+
+
+@app.post("/books")
+def add_new(book_json: Book, filename: str = 'books.json'):
+    if not os.path.exists(filename):
+        data = {}
+    else:
+        with open(filename, 'r', encoding='utf-8') as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                data = {}
+
+    data[str(book_json.id)] = book_json.dict()
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+    return {"message": f"Книга '{book_json.title}' додана!"}
